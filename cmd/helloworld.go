d0a2b572b1b88b80cc3058c4cf237d2bf9e39ef5
kizunai9 <yaodong.sun@outlook.com>
2023-06-20 21:28:32 +0200

    dd

--- cmd/helloworld.go
+++ cmd/helloworld.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
+
 	fmt.Println("hello world")
 	s := mymath.Add(3, 4)
-	fmt.Printf("s = %v", s)
+	fmt.Printf("s = %v\n", s)
+	fmt.Println("------")
+	fmt.Println(intAdd7(2, 3))
 	fmt.Println("Press Enter to exit...")
 	fmt.Scanln()
+}
+
+func intAdd7(a int, b int) int {
+	var c int = 7
+	var gg float64 = 2.3
+	f := func(o int) int { return o }
+	return f(c) + b + int(gg)
 }
