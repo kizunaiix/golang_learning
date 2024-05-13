390956b43423fc42b5638a241d63530f69fcc7a9
kizunai9 <yaodong.sun@outlook.com>
2024-05-13 21:43:33 +0200

    ok

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("hello")
-	list1 := [...]string{"abc", "def", "ghi"}
-	slice1 := list1[:2]
-	fmt.Printf("slice1 : %v \n", slice1)
 
 }
