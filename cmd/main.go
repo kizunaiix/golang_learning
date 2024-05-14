57f452ec665ee5c9bbdff58820811a9924a16d10
kizunai9 <yaodong.sun@outlook.com>
2024-05-14 21:35:53 +0200

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+// Barkable 接口
+// type Barkable interface {
+// 	(any) bark()
+// }
+
+type Animal struct {
+	Dog
+	Duck
+}
+
+// Dog 结构体
+type Dog struct{}
+
+// Duck 结构体
+type Duck struct{}
+
+// Dog 实现 Barkable 接口的 bark 方法
+func (d Dog) bark() {
+	fmt.Println("wang!")
+}
+
+// Duck 实现 Barkable 接口的 bark 方法
+// func (d Animal) bark() {
+// 	fmt.Println("gua!")
+// }
+
+func main() {
+	b1 := Animal{}
+	b1.bark() // 输出: wang!
 }
