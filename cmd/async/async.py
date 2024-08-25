fb3f57f4274ccd583a5ed430793b81c91b3ef322
kizunai9 <yaodong.sun@outlook.com>
2024-08-25 21:12:20 +0200

    pyasync

--- cmd/async/async.py
+++ cmd/async/async.py
@@ -1,11 +1,20 @@
-from asyncio import sleep, run
+import asyncio
 import time
 
-async def my_async_func():
-    time.sleep(3)
-    print("wake!")
+async def my_async_func(seconds: int):
+    await asyncio.sleep(seconds)
+    print(f"wake after {seconds} !")
 
+async def main():
+    print(f"[{time.asctime()}] start")
 
-print("start")
-run(my_async_func())
-print("end.")
+    # 并发运行两个 my_async_func 函数
+    await asyncio.gather(
+        my_async_func(3),
+        my_async_func(5),
+    )
+
+    print(f"[{time.asctime()}] end.")
+
+# 运行异步主函数
+asyncio.run(main())
